537e821a9e35496e6c92f3b833ed7c23025d19d5
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2022-08-23 10:32:54 +0200

    remove logging error in tests

--- tests/config/checker/checkers/test_checker.py
+++ tests/config/checker/checkers/test_checker.py
@@ -8,7 +8,7 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
-
+import logging
 import os
 from unittest import mock
 from unittest.mock import MagicMock
@@ -37,8 +37,9 @@ class TestChecker:
         assert len(Config._collector.errors) == 0
 
         Config.global_config.clean_entities_enabled = "foo"
-        with pytest.raises(ConfigurationIssueError):
-            Config.check()
+        with mock.patch.object(logging.Logger, 'error') as error_message:
+            with pytest.raises(ConfigurationIssueError):
+                Config.check()
         assert len(Config._collector.errors) == 1
 
         Config.global_config.clean_entities_enabled = GlobalAppConfig._CLEAN_ENTITIES_ENABLED_TEMPLATE
@@ -52,12 +53,13 @@ class TestChecker:
             Config.check()
             assert len(Config._collector.errors) == 0
 
-        with mock.patch.dict(os.environ, {"FOO": "foo"}):
-            Config.global_config.clean_entities_enabled = "ENV[FOO]"
-            Config._collector = IssueCollector()
-            with pytest.raises(ConfigurationIssueError):
-                Config.check()
-            assert len(Config._collector.errors) == 1
+        with mock.patch.object(logging.Logger, 'error') as error_message:
+            with mock.patch.dict(os.environ, {"FOO": "foo"}):
+                Config.global_config.clean_entities_enabled = "ENV[FOO]"
+                Config._collector = IssueCollector()
+                with pytest.raises(ConfigurationIssueError):
+                    Config.check()
+                assert len(Config._collector.errors) == 1
 
     def test_register_checker(self):
         checker = CheckerForTest
