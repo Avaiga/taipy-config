89c6e1e4463978f1f12bc2b4767f507b323ce896
trgiangdo <dtr.giang.1299@gmail.com>
2023-03-06 17:33:43 +0700

    feat: add methods to unblock a section and log message on _ConfigComparator

--- src/taipy/config/_config_comparator.py
+++ src/taipy/config/_config_comparator.py
@@ -11,9 +11,11 @@
 
 import json
 import re
+from typing import Dict, List, Union
 
 from deepdiff import DeepDiff
 
+from ..logger._taipy_logger import _TaipyLogger
 from ._base_serializer import _BaseSerializer
 from ._config import _Config
 from .config import Config
@@ -26,20 +28,29 @@ class _ConfigComparator(dict):
     ```python
     {
         "added_items": [
-            ((entity_name_1, entity_id_1, attribute_1), added_object_1),
-            ((entity_name_2, entity_id_2, attribute_2), added_object_2),
+            ((section_name_1, config_id_1, attribute_1), added_object_1),
+            ((section_name_2, config_id_2, attribute_2), added_object_2),
         ],
         "removed_items": [
-            ((entity_name_1, entity_id_1, attribute_1), removed_object_1),
-            ((entity_name_2, entity_id_2, attribute_2), removed_object_2),
+            ((section_name_1, config_id_1, attribute_1), removed_object_1),
+            ((section_name_2, config_id_2, attribute_2), removed_object_2),
         ],
         "modified_items": [
-            ((entity_name_1, entity_id_1, attribute_1), (old_value_1, new_value_1)),
-            ((entity_name_2, entity_id_2, attribute_2), (old_value_2, new_value_2)),
+            ((section_name_1, config_id_1, attribute_1), (old_value_1, new_value_1)),
+            ((section_name_2, config_id_2, attribute_2), (old_value_2, new_value_2)),
         ],
     }
     ```
     """
+
+    _ADDED_ITEMS_KEY = "added_items"
+    _REMOVED_ITEMS_KEY = "removed_items"
+    _MODIFIED_ITEMS_KEY = "modified_items"
+
+    _BLOCKED_SECTION_KEY = "blocked_diff_sections"
+    _UNBLOCKED_SECTION_KEY = "unblocked_diff_sections"
+
+    _UNBLOCKED_SECTIONS = set()
 
     def __init__(self, old_conf: _Config, new_conf: _Config):
         super().__init__()
@@ -49,57 +60,69 @@ class _ConfigComparator(dict):
 
         self.deepdiff_result = DeepDiff(self.old_conf_json, self.new_conf_json)
 
-        self.__setitem__("added_items", [])
-        self.__setitem__("removed_items", [])
-        self.__setitem__("modified_items", [])
+        self.__check_added_items()
+        self.__check_removed_items()
+        self.__check_modified_items()
 
-        self._check_added_items()
+        self.__sort_by_section()
+        self.__logger = _TaipyLogger._get_logger()
 
-        self._check_removed_items()
+    def __sort_by_section(self):
+        if self.get(self._BLOCKED_SECTION_KEY):
+            for key in self[self._BLOCKED_SECTION_KEY].keys():
+                self[self._BLOCKED_SECTION_KEY][key].sort(key=lambda x: x[0][0])
 
-        self._check_modified_items()
+        if self.get(self._UNBLOCKED_SECTION_KEY):
+            for key in self[self._UNBLOCKED_SECTION_KEY].keys():
+                self[self._UNBLOCKED_SECTION_KEY][key].sort(key=lambda x: x[0][0])
 
-        # Sort by entity name
-        self["added_items"].sort(key=lambda x: x[0][0])
-        self["removed_items"].sort(key=lambda x: x[0][0])
-        self["modified_items"].sort(key=lambda x: x[0][0])
-
-    def _check_added_items(self):
+    def __check_added_items(self):
         if dictionary_item_added := self.deepdiff_result.get("dictionary_item_added"):
             for item_added in dictionary_item_added:
-                entity_name, entity_id, attribute = self.__get_changed_entity_attribute(item_added)
+                section_name, config_id, attribute = self.__get_changed_entity_attribute(item_added)
+                diff_sections = self.__get_section(section_name)
 
                 if attribute:
-                    value_added = self.new_conf_json[entity_name][entity_id][attribute]
+                    value_added = self.new_conf_json[section_name][config_id][attribute]
                 else:
-                    value_added = self.new_conf_json[entity_name][entity_id]
+                    value_added = self.new_conf_json[section_name][config_id]
 
-                entity_name = self.__rename_global_node_name(entity_name)
+                section_name = self.__rename_global_node_name(section_name)
+                self.__create_or_append_list(
+                    diff_sections,
+                    self._ADDED_ITEMS_KEY,
+                    ((section_name, config_id, attribute), (value_added)),
+                )
 
-                self["added_items"].append(((entity_name, entity_id, attribute), (value_added)))
-
-    def _check_removed_items(self):
+    def __check_removed_items(self):
         if dictionary_item_removed := self.deepdiff_result.get("dictionary_item_removed"):
             for item_removed in dictionary_item_removed:
-                entity_name, entity_id, attribute = self.__get_changed_entity_attribute(item_removed)
+                section_name, config_id, attribute = self.__get_changed_entity_attribute(item_removed)
+                diff_sections = self.__get_section(section_name)
 
                 if attribute:
-                    value_removed = self.old_conf_json[entity_name][entity_id][attribute]
+                    value_removed = self.old_conf_json[section_name][config_id][attribute]
                 else:
-                    value_removed = self.old_conf_json[entity_name][entity_id]
+                    value_removed = self.old_conf_json[section_name][config_id]
 
-                entity_name = self.__rename_global_node_name(entity_name)
+                section_name = self.__rename_global_node_name(section_name)
+                self.__create_or_append_list(
+                    diff_sections,
+                    self._REMOVED_ITEMS_KEY,
+                    ((section_name, config_id, attribute), (value_removed)),
+                )
 
-                self["removed_items"].append(((entity_name, entity_id, attribute), (value_removed)))
-
-    def _check_modified_items(self):
+    def __check_modified_items(self):
         if values_changed := self.deepdiff_result.get("values_changed"):
             for item_changed, value_changed in values_changed.items():
-                entity_name, entity_id, attribute = self.__get_changed_entity_attribute(item_changed)
-                entity_name = self.__rename_global_node_name(entity_name)
+                section_name, config_id, attribute = self.__get_changed_entity_attribute(item_changed)
+                diff_sections = self.__get_section(section_name)
 
-                self["modified_items"].append(
-                    ((entity_name, entity_id, attribute), (value_changed["old_value"], value_changed["new_value"]))
+                section_name = self.__rename_global_node_name(section_name)
+                self.__create_or_append_list(
+                    diff_sections,
+                    self._MODIFIED_ITEMS_KEY,
+                    ((section_name, config_id, attribute), (value_changed["old_value"], value_changed["new_value"])),
                 )
 
         # Iterable item added will be considered a modified item
@@ -112,31 +135,96 @@ class _ConfigComparator(dict):
 
     def __check_modified_iterable(self, iterable_items):
         for item in iterable_items:
-            entity_name, entity_id, attribute = self.__get_changed_entity_attribute(item)
+            section_name, config_id, attribute = self.__get_changed_entity_attribute(item)
+            diff_sections = self.__get_section(section_name)
 
             if attribute:
-                new_value = self.new_conf_json[entity_name][entity_id][attribute]
-                old_value = self.old_conf_json[entity_name][entity_id][attribute]
+                new_value = self.new_conf_json[section_name][config_id][attribute]
+                old_value = self.old_conf_json[section_name][config_id][attribute]
             else:
-                new_value = self.new_conf_json[entity_name][entity_id]
-                old_value = self.old_conf_json[entity_name][entity_id]
+                new_value = self.new_conf_json[section_name][config_id]
+                old_value = self.old_conf_json[section_name][config_id]
 
-            entity_name = self.__rename_global_node_name(entity_name)
+            section_name = self.__rename_global_node_name(section_name)
+            modified_value = ((section_name, config_id, attribute), (old_value, new_value))
 
-            if not ((entity_name, entity_id, attribute), (old_value, new_value)) in self["modified_items"]:
-                self["modified_items"].append(((entity_name, entity_id, attribute), (old_value, new_value)))
+            if modified_value not in diff_sections[self._MODIFIED_ITEMS_KEY]:
+                self.__create_or_append_list(
+                    diff_sections,
+                    self._MODIFIED_ITEMS_KEY,
+                    modified_value,
+                )
+
+    def __get_section(self, section_name: str) -> Dict[str, List]:
+        if section_name in self._UNBLOCKED_SECTIONS:
+            if not self.get(self._UNBLOCKED_SECTION_KEY):
+                self[self._UNBLOCKED_SECTION_KEY] = {}
+            return self[self._UNBLOCKED_SECTION_KEY]
+
+        if not self.get(self._BLOCKED_SECTION_KEY):
+            self[self._BLOCKED_SECTION_KEY] = {}
+        return self[self._BLOCKED_SECTION_KEY]
+
+    def __create_or_append_list(self, diff_dict, key, value):
+        if diff_dict.get(key):
+            diff_dict[key].append(value)
+        else:
+            diff_dict[key] = [value]
 
     def __get_changed_entity_attribute(self, attribute_bracket_notation):
         """Split the entity name, entity id (if exists), and the attribute name from JSON bracket notation."""
         try:
-            entity_name, entity_id, attribute = re.findall(r"\[\'(.*?)\'\]", attribute_bracket_notation)
+            section_name, config_id, attribute = re.findall(r"\[\'(.*?)\'\]", attribute_bracket_notation)
         except ValueError:
-            entity_name, entity_id = re.findall(r"\[\'(.*?)\'\]", attribute_bracket_notation)
+            section_name, config_id = re.findall(r"\[\'(.*?)\'\]", attribute_bracket_notation)
             attribute = None
 
-        return entity_name, entity_id, attribute
+        return section_name, config_id, attribute
 
     def __rename_global_node_name(self, node_name):
         if node_name == _BaseSerializer._GLOBAL_NODE_NAME:
             return "Global Configuration"
         return node_name
+
+    def _log_blocked_sections(self):
+        if blocked_sections := self.get(self._BLOCKED_SECTION_KEY):
+            self.__log_message(blocked_sections, self.__logger.error)
+
+    def _log_unblocked_sections(self):
+        if unblocked_sections := self.get(self._UNBLOCKED_SECTION_KEY):
+            self.__log_message(unblocked_sections, self.__logger.info)
+
+    def __log_message(self, diff_sections, log_fct):
+        dq = '"'
+
+        if added_items := diff_sections.get(self._ADDED_ITEMS_KEY):
+            for diff in added_items:
+                ((section_name, config_id, attribute), added_object) = diff
+                log_fct(
+                    f"{section_name} {dq}{config_id}{dq} "
+                    f"{f'has attribute {dq}{attribute}{dq}' if attribute else 'was'} added: {added_object}"
+                )
+
+        if removed_items := diff_sections.get(self._REMOVED_ITEMS_KEY):
+            for diff in removed_items:
+                ((section_name, config_id, attribute), removed_object) = diff
+                log_fct(
+                    f"{section_name} {dq}{config_id}{dq} "
+                    f"{f'has attribute {dq}{attribute}{dq}' if attribute else 'was'} removed"
+                )
+
+        if modified_items := diff_sections.get(self._MODIFIED_ITEMS_KEY):
+            for diff in modified_items:
+                ((section_name, config_id, attribute), (old_value, new_value)) = diff
+                log_fct(
+                    f"{section_name} {dq}{config_id}{dq} "
+                    f"{f'has attribute {dq}{attribute}{dq}' if attribute else 'was'} modified: "
+                    f"{old_value} -> {new_value}"
+                )
+
+    @classmethod
+    def _unblock_section(cls, section_name: Union[str, List[str]]):
+        if isinstance(section_name, str):
+            section_name = [section_name]
+
+        cls._UNBLOCKED_SECTIONS.update(section_name)
--- tests/config/test_config_comparator.py
+++ tests/config/test_config_comparator.py
@@ -16,7 +16,7 @@ from tests.config.utils.section_for_tests import SectionForTest
 from tests.config.utils.unique_section_for_tests import UniqueSectionForTest
 
 
-def test_config_comparator():
+def test_config_comparator_return():
     unique_section_1 = UniqueSectionForTest(attribute="unique_attribute_1", prop="unique_prop_1")
     section_1 = SectionForTest("section_1", attribute="attribute_1", prop="prop_1")
     section_2a = SectionForTest("section_2", attribute=2, prop="prop_2")
@@ -34,6 +34,9 @@ def test_config_comparator():
         "section_5": section_5a,
     }
     _config_1._unique_sections[UniqueSectionForTest.name] = unique_section_1
+
+    config_diff = _ConfigComparator(_config_1, _config_1)
+    assert config_diff == {}
 
     _config_2 = _Config._default_config()
     # Update global config
@@ -54,41 +57,119 @@ def test_config_comparator():
     _config_2._unique_sections[UniqueSectionForTest.name] = unique_section_1
 
     config_diff = _ConfigComparator(_config_1, _config_2)
+    assert config_diff.get("unblocked_diff_sections") is None
+    blocked_config_diff = config_diff["blocked_diff_sections"]
 
     # The result was sorted so test by indexing is fine.
-    assert len(config_diff["added_items"]) == 2
-    assert config_diff["added_items"][1] == (
+    assert len(blocked_config_diff["added_items"]) == 2
+    assert blocked_config_diff["added_items"][1] == (
         ("section_name", "section_4", None),
         {"attribute": "4:int", "prop": "prop_4"},
     )
+    assert blocked_config_diff["added_items"][0] == (
+        ("Global Configuration", "repository_properties", None),
+        {"foo": "bar"},
+    )
 
-    # TODO: This should not in ["added_items"] since it is a default value that was changed. This should be fixed soon.
-    assert config_diff["added_items"][0] == (("Global Configuration", "repository_properties", None), {"foo": "bar"})
-
-    assert len(config_diff["modified_items"]) == 8
-    assert config_diff["modified_items"][0] == (("Global Configuration", "root_folder", None), ("./taipy/", "foo"))
-    assert config_diff["modified_items"][1] == (("Global Configuration", "storage_folder", None), (".data/", "bar"))
-    assert config_diff["modified_items"][2] == (
+    assert len(blocked_config_diff["modified_items"]) == 8
+    assert blocked_config_diff["modified_items"][0] == (
+        ("Global Configuration", "root_folder", None),
+        ("./taipy/", "foo"),
+    )
+    assert blocked_config_diff["modified_items"][1] == (
+        ("Global Configuration", "storage_folder", None),
+        (".data/", "bar"),
+    )
+    assert blocked_config_diff["modified_items"][2] == (
         ("Global Configuration", "clean_entities_enabled", None),
         ("ENV[TAIPY_CLEAN_ENTITIES_ENABLED]", "True:bool"),
     )
-    assert config_diff["modified_items"][3] == (
+    assert blocked_config_diff["modified_items"][3] == (
         ("Global Configuration", "repository_type", None),
         ("filesystem", "baz"),
     )
-    assert config_diff["modified_items"][4] == (("section_name", "section_2", "attribute"), ("2:int", "20:int"))
-    assert config_diff["modified_items"][5] == (("section_name", "section_2", "prop"), ("prop_2", "prop_2b"))
-    assert config_diff["modified_items"][6] == (
+    assert blocked_config_diff["modified_items"][4] == (("section_name", "section_2", "attribute"), ("2:int", "20:int"))
+    assert blocked_config_diff["modified_items"][5] == (("section_name", "section_2", "prop"), ("prop_2", "prop_2b"))
+    assert blocked_config_diff["modified_items"][6] == (
         ("section_name", "section_5", "prop"),
         (["prop_5"], ["prop_4", "prop_5"]),
     )
-    assert config_diff["modified_items"][7] == (
+    assert blocked_config_diff["modified_items"][7] == (
         ("section_name", "section_5", "attribute"),
         (["1:int", "2:int", "3:int", "4:int", "5:int"], ["1:int", "2:int"]),
     )
 
-    assert len(config_diff["removed_items"]) == 1
-    assert config_diff["removed_items"][0] == (
+    assert len(blocked_config_diff["removed_items"]) == 1
+    assert blocked_config_diff["removed_items"][0] == (
         ("section_name", "section_3", None),
         {"attribute": "3:int", "prop": "prop_3"},
     )
+
+
+def test_unblock_section_when_compare(caplog):
+    unique_section_1 = UniqueSectionForTest(attribute="unique_attribute_1", prop="unique_prop_1")
+    section_1 = SectionForTest("section_1", attribute="attribute_1", prop="prop_1")
+    section_2 = SectionForTest("section_2", attribute=2, prop="prop_2")
+
+    _config_1 = _Config._default_config()
+    _config_1._sections[SectionForTest.name] = {"section_1": section_1}
+    _config_1._unique_sections[UniqueSectionForTest.name] = unique_section_1
+
+    _config_2 = _Config._default_config()
+    _config_2._global_config = GlobalAppConfig.default_config()
+    _config_2._global_config.repository_properties = {"foo": "bar"}
+    _config_2._sections[SectionForTest.name] = {"section_1": section_2}
+    _config_2._unique_sections[UniqueSectionForTest.name] = unique_section_1
+
+    # Compare 2 Configuration
+    config_diff = _ConfigComparator(_config_1, _config_2)
+
+    config_diff._log_unblocked_sections()
+    assert caplog.text == ""
+    assert config_diff.get("unblocked_diff_sections") is None
+    caplog.clear()
+
+    blocked_config_diff = config_diff["blocked_diff_sections"]
+    config_diff._log_blocked_sections()
+    assert len(caplog.text.strip().split("\n")) == 3
+    assert len(blocked_config_diff["added_items"]) == 1
+    assert len(blocked_config_diff["modified_items"]) == 2
+    assert blocked_config_diff.get("removed_items") is None
+    caplog.clear()
+
+    # Ignore any diff of "section_name" and compare
+    _ConfigComparator._unblock_section("section_name")
+    config_diff = _ConfigComparator(_config_1, _config_2)
+
+    config_diff._log_blocked_sections()
+    blocked_config_diff = config_diff["blocked_diff_sections"]
+    assert len(caplog.text.strip().split("\n")) == 1
+    assert len(blocked_config_diff["added_items"]) == 1
+    assert blocked_config_diff.get("modified_items") is None
+    assert blocked_config_diff.get("removed_items") is None
+    caplog.clear()
+
+    config_diff._log_unblocked_sections()
+    unblocked_config_diff = config_diff["unblocked_diff_sections"]
+    assert len(caplog.text.strip().split("\n")) == 2
+    assert unblocked_config_diff.get("added_items") is None
+    assert len(unblocked_config_diff["modified_items"]) == 2
+    assert unblocked_config_diff.get("removed_items") is None
+    caplog.clear()
+
+    # Ignore any diff of Global Config and compare
+    _ConfigComparator._unblock_section(["TAIPY"])
+    config_diff = _ConfigComparator(_config_1, _config_2)
+
+    config_diff._log_blocked_sections()
+    assert caplog.text == ""
+    assert config_diff.get("blocked_diff_sections") is None
+    caplog.clear()
+
+    unblocked_config_diff = config_diff["unblocked_diff_sections"]
+    config_diff._log_unblocked_sections()
+    assert len(caplog.text.strip().split("\n")) == 3
+    assert len(unblocked_config_diff["added_items"]) == 1
+    assert len(unblocked_config_diff["modified_items"]) == 2
+    assert unblocked_config_diff.get("removed_items") is None
+    caplog.clear()
